8176bb817cee5185031ed3a6cdccae288ffc6823
HossamBlack0 <ghossam558877@gmail.com>
2023-08-23 18:41:23 +0300

    Task

--- Task/Task 1.py
+++ Task/Task 1.py
@@ -1,1 +1,6 @@
-print ("Hello World!")
+def reverse_words(sentence):
+    return ' '.join(word[::-1] for word in sentence.split())
+
+input_sentence = "Hello World"
+output_sentence = reverse_words(input_sentence)
+print(output_sentence)
